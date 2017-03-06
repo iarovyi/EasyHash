0ec3e21a6b3abf61f53d23e4755fccbe1fd390a6
Sergii <serge.zp.ua@gmail.com>
2017-03-06 19:43:19 +0100

    optimize debug script

--- build.debug.ps1
+++ build.debug.ps1
@@ -1,14 +1,2 @@
-$TOOLS_DIR = Join-Path $PSScriptRoot "tools"
-$CAKE_DIR = Join-Path $TOOLS_DIR "Cake"
-$CAKE_EXE = Join-Path $CAKE_DIR "Cake.exe"
-
-if (-Not (Test-Path $CAKE_EXE)) {
-	Write-Host "Cake is not installed. Running normal build";
-	& ".\build.ps1"
-}
-
-Write-Host "Debugging Cake in Visual Studio under Administration Priviliges with opened 'build.cake' file ..."	 
-Push-Location $CAKE_DIR
-.\Cake.exe ..\..\build.cake -target=Default --debug
-Pop-Location
-exit $LASTEXITCODE
+Write-Host "Debugging Cake in Visual Studio under Administration Priviliges with opened 'build.cake' file ..."
+Invoke-Expression ".\build.ps1 -ScriptArgs '--debug'"
